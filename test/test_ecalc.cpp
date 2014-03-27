ceb1e94680c1a3c19fbb5260309b4a6417106ec4
pandaant <apandaant@gmail.com>
2014-03-27 13:15:08 +0000

    libecalc -> equity is wrong in some tests

--- test/test_ecalc.cpp
+++ test/test_ecalc.cpp
@@ -21,16 +21,58 @@ SUITE(ECalcTests) {
     handlist_collection hands(
         {ECalc::random_handlist(0), ECalc::random_handlist(0)});
     result_collection res = calc.evaluate(hands, board, dead, NB_SAMPLES);
-    CHECK_CLOSE(0.5, res[0].pwin_tie(), 0.05);
+    CHECK_CLOSE(0.5, res[0].pwin_tie(), 0.02);
   }
 
   TEST_FIXTURE(Setup, EvaluateSingleVsRandom) {
-
     cards board, dead;
     result_collection res = calc.evaluate_vs_random(
         ECalc::single_handlist(Hand("AhAs")), 1, board, dead, NB_SAMPLES);
-    CHECK_CLOSE(0.85204, res[0].pwin_tie(), 0.05);
+    CHECK_CLOSE(0.85204, res[0].pwin_tie(), 0.02);
+    CHECK_CLOSE(1-res[0].pwin_tie(), res[1].pwin_tie(), 0.02);
   }
+
+  TEST_FIXTURE(Setup, AcKdVSJsTs) {
+    cards dead;
+    cards board(
+        {(card)Card("Jh").get_card(), (card)Card("Qd").get_card(), (card)Card("Kh").get_card()});
+    handlist_collection hands(
+        {ECalc::single_handlist(Hand("AcKd")), ECalc::single_handlist(Hand("JsTs"))});
+    result_collection res = calc.evaluate(hands, board, dead, NB_SAMPLES);
+    CHECK_CLOSE(0.66313, res[0].pwin_tie(), 0.02);
+    CHECK_CLOSE(0.33687, res[1].pwin_tie(), 0.02);
+  }
+
+  TEST_FIXTURE(Setup, AcKdVSQQplus) {
+    cards dead;
+    cards board(
+        {(card)Card("Jh").get_card(), (card)Card("Qs").get_card(), (card)Card("Kc").get_card()});
+
+    handlist opp_range({ECalc::create_hand(Hand("AhAs")),
+                        ECalc::create_hand(Hand("KhKs")),
+                        ECalc::create_hand(Hand("QcQd"))});
+    handlist_collection hands(
+        {ECalc::single_handlist(Hand("AcKd")), opp_range});
+    result_collection res = calc.evaluate(hands, board, dead, NB_SAMPLES);
+    CHECK_CLOSE(0.15118, res[0].pwin_tie(), 0.02);
+    CHECK_CLOSE(0.84882, res[1].pwin_tie(), 0.02);
+  }
+
+  //TEST_FIXTURE(Setup, AcKdVSQQplusDeadcards) {
+    //cards dead(
+        //{(card)Card("Jh").get_card(), (card)Card("Qd").get_card(), (card)Card("Kc").get_card()});
+    //cards board(
+        //{(card)Card("Jh").get_card(), (card)Card("Qd").get_card(), (card)Card("Kc").get_card()});
+
+    //handlist opp_range({ECalc::create_hand(Hand("AhAs")),
+                        //ECalc::create_hand(Hand("KhKs")),
+                        //ECalc::create_hand(Hand("QcQd"))});
+    //handlist_collection hands(
+        //{ECalc::single_handlist(Hand("AcKd")), opp_range});
+    //result_collection res = calc.evaluate(hands, board, dead, NB_SAMPLES);
+    //CHECK_CLOSE(0.15101, res[0].pwin_tie(), 0.02);
+    //CHECK_CLOSE(0.84899, res[1].pwin_tie(), 0.02);
+  //}
 
   /**
     * Special case when 2 or more players
