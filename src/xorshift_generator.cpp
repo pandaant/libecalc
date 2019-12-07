afa0e240ffd88d568903acd80bd7429cc5273064
strikles <strikles@gmail.com>
2019-12-07 20:49:43 +0000

    fix xorshift_generator compile error

--- src/xorshift_generator.cpp
+++ src/xorshift_generator.cpp
@@ -1,8 +1,8 @@
 #include "xorshift_generator.hpp"
 
 namespace ecalc {
-const uint32_t XOrShiftGenerator::MAX = std::numeric_limits<uint32_t>::max();
-const uint32_t XOrShiftGenerator::MIN = std::numeric_limits<uint32_t>::min();
+//const uint32_t XOrShiftGenerator::MAX = std::numeric_limits<uint32_t>::max();
+//const uint32_t XOrShiftGenerator::MIN = std::numeric_limits<uint32_t>::min();
 
 XOrShiftGenerator::XOrShiftGenerator(const uint32_t &seed_) {
   x = 123456789;
